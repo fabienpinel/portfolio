43a3d8fe5fa779508e7ee3c29ebae346c0ff0b06
Fabien Pinel <fabienpinel@yahoo.fr>
2017-02-03 13:12:24 +0100

    Initial react architecture

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: [
+        "./src/app.js"
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders:[
+            {
+                test: /\.jsx?$/,         // Match both .js and .jsx files
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query:
+                {
+                    presets:['es2015','react']
+                }
+            }
+        ]
+    },
+    plugins: [
+    ]
+
+};
