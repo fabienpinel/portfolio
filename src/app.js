991be8635c4135f4e6f1d46daa050f8906ec1776
Fabien Pinel <fabienpinel@yahoo.fr>
2017-02-08 22:28:41 +0100

    tagline, button

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom'
 import { IndexRoute,Router, Route,hashHistory} from 'react-router';
 
-require('./assets/sass/reset.scss');
 
 /**
  * Component imports
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Data from './data';
 import Header from './header';
 
+require('../assets/sass/reset.scss');
 require('../assets/sass/index.scss');
 
 /**
@@ -22,6 +23,7 @@ let Index = React.createClass({
                 <section id="presentation">
                     <img src="../other/logo/fp-logo_second.jpg" id="logo" />
                     <p id="tagline">I am a developper, runner and performance addict</p>
+                    <button id="action" >who am I ?</button>
                 </section>
             </section>
         );
